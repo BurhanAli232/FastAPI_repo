23a3b837c27b962cec665e5a4176aef1adf2492c
burhanali232 <burhanali232@gmail.com>
2025-10-11 20:51:40 -0700

    Path parameter

--- Day_3/path_query_parameter.py
+++ Day_3/path_query_parameter.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app=FastAPI()
 
-def reaad_data():
-    with open('FastAPI_repo\data.json') as f:
-        data=json.load(f)
-    return data
+def read_data():
+    with open(r'C:\Users\Burhan Zulfiqar\Music\FastAPI_repo\data.json') as f:
+        patient_data=json.load(f)
+    return patient_data
 
 @app.get('/')
 def Home():
@@ -18,4 +18,21 @@ def blog():
 
 @app.get('/data')
 def get_data():
-    return reaad_data()
+    return read_data()
+
+@app.get('/patient/{id}')
+def view_patient(id: str = Path(...,
+                                Name="Patient ID",
+                                 description="The ID of the patient you want to view",
+                                 message="Patient ID must be a string",
+                                 example="A001",
+                                 min_length=4,
+                                 max_length=4,
+                                 regex="^[A-Z][0-9]{3}$"
+
+                                 )):
+    patient=read_data()
+    for i in patient:
+        if i["id"]==id:
+            return i
+    raise HTTPException(status_code=404,detail="You entered wrong ID Please enter a valid id and again try")
